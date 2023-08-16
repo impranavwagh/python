f03a09645ddb03ac9affabf03847ba4f4bd38a4e
impranavwagh <120593949+impranavwagh@users.noreply.github.com>
2023-08-16 19:34:29 +0530

    Update Integrate GPS co-ordinate with Python.py

--- Integrate GPS co-ordinate with Python.py
+++ Integrate GPS co-ordinate with Python.py
@@ -1,63 +1,18 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[4]:
-
-
 import geopy
-
-
-# In[5]:
-
-
 import folium
 
-
-# In[9]:
-
-
+# for finding location by latitude and longitude
 geolocator = geopy.Nominatim(user_agent="GetLoc")
 
-
-# In[10]:
-
-
 locname = geolocator.reverse("26.92552825725126, 75.77955512259769")
-
-
-# In[11]:
-
 
 print(locname)
 
 
-# In[ ]:
 
-
-
-
-
-# In[14]:
-
-
-
-
-
-# In[19]:
-
-
-name = geolocator.geocode("Mumbai")
-
-
-# In[20]:
-
+#finding longitude and latitude by name of area
+name = geolocator. Geocode("Mumbai")
 
 #it will print latitude and longitude
-print(name.latitude, name.longitude)
-
-
-# In[ ]:
-
-
-
+print(name.latitude, name. Longitude)
 
